18decc966737a561adc7462edac8df258caefd07
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2026-09-09 16:06:13 -0300

    Add LetoDB project & zip/unzip module
    Add LetoDB build helpers, Harbour project files, and a zip/unzip module. Introduces fazleto32.bat and fazleto64.bat, project manifests (leto.hbp, letodb.hbp, rddleto.hbp) and letozipmod.prg implementing leto_Zip and leto_UnZip (uses hb_ZipFile/hb_UnzipFile). Project files include common sources, blowfish, platform-specific flags, and optional ZIP/BM integrations; they reference $hb_pkg_install.hbm for packaging.

---
+++ leto/fazleto64.bat
@@ -0,0 +1,9 @@
+call d:\devprg\hb64\hb64msys.bat
+call c:\devprg\hb64\hb64msys_C.bat
+REM SET HB_INSTALL_PREFIX=c:\harbour\LetoDBf\hb64\
+hbmk2 leto.hbp
+hbmk2 letodb.hbp
+hbmk2 letodyn.hbp
+hbmk2 rddleto.hbp
+
+
