43f788e1a2615ac181cd254215727a550e8a4571
jcassianojr <47321671+jcassianojr@users.noreply.github.com>
2026-09-10 19:11:44 -0300

    Add Elch build support and dyn library
    This change comments out the pkg-install hooks in the main Leto HBP files, adds a new dynamic-library build target for leto, and introduces the Elch-specific build scripts and RDD configuration needed to compile the Elch variant of the Leto client and RDD.

---
+++ letoelch/fazleto32elch.bat
@@ -0,0 +1,11 @@
+call \devprg\hb\hb32msys.bat
+call \devprg\hb\hb32msys_c.bat
+rem SET HB_INSTALL_PREFIX=c:\harbour\LetoDBf\hb32\
+rem hbmk2 apileto.hbp
+rem hbmk2 letodb.hbp
+rem hbmk2 letodbaddon.hbp
+hbmk2 letodbsvc.hbp
+rem hbmk2 rddleto.hbp
+hbmk2 rddletoelch.hbp
+rem hbmk2 rddletoaddon.hbp
+
---
+++ letoelch/fazleto64elch.bat
@@ -0,0 +1,11 @@
+call d:\devprg\hb64\hb64msys.bat
+call c:\devprg\hb64\hb64msys_C.bat
+rem SET HB_INSTALL_PREFIX=c:\harbour\LetoDBf\hb64\
+rem hbmk2 apileto.hbp
+rem hbmk2 letodb.hbp
+rem hbmk2 letodbaddon.hbp
+hbmk2 letodbsvc.hbp
+rem hbmk2 rddleto.hbp
+rem hbmk2 rddletoaddon.hbp
+hbmk2 rddletoelch.hbp
+
